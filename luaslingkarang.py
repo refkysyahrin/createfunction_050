46f235b0506a5c2fd132b513d5f95d2f2d8d1ed8
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-10-21 22:54:27 +0700

    luas lingkaran

---
+++ luaslingkarang.py
@@ -0,0 +1,5 @@
+luas_lingkaran = lambda r: 3.14 * r ** 2
+
+jari_jari = float(input("Masukkan jari-jari lingkaran: "))
+
+print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah {luas_lingkaran(jari_jari)}")
